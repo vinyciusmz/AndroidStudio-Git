0810f660d2bdfd95094e16a3a38fb6105c028b58
Zanardi <zanardimz@gmail.com>
2022-04-08 20:00:09 -0300

    Modificação no layout - Novo botão

---
+++ app/src/main/java/com/example/aula09/AtividadeTeste.kt
@@ -0,0 +1,11 @@
+package com.example.aula09
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AtividadeTeste : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_atividade_teste)
+    }
+}
